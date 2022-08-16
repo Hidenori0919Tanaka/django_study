0ff6bcc3b5e66259f55fc70bb1516b747040ad5f
TNK_Hidy <hidenori091985@yahoo.co.jp>
2022-08-16 11:39:45 +0900

    add unit1

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'pygments_renderer',
     'snippets.apps.SnippetsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'djangosnippets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ユーザー認証
+#
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
