4562d16db98fcef566b0461e419f90b592bc2913
TNK_Hidy <hidenori091985@yahoo.co.jp>
2022-09-10 17:42:53 +0900

    add cpmmentform

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.utils.translation import gettext as _
 
-from snippets.forms import SnippetForm
-from snippets.models import Snippet
+from snippets.forms import SnippetForm, CommentForm
+from snippets.models import Snippet, Comment
 
 
 def top(request):
@@ -20,7 +21,12 @@ def snippet_new(request):
             snippet = form.save(commit=False)
             snippet.created_by = request.user
             snippet.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 _("Success to create a new snippet."))
             return redirect(snippet_detail, snippet_id=snippet.pk)
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 _("Failed to create a new snippet."))
     else:
         form = SnippetForm()
     return render(request, "snippets/snippet_new.html", {'form': form})
@@ -30,25 +36,49 @@ def snippet_new(request):
 def snippet_edit(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     if snippet.created_by_id != request.user.id:
-        return HttpResponseForbidden("このスニペットの編集は許可されていません。")
+        return HttpResponseForbidden(_("You cannot to edit this snippet."))
 
     if request.method == "POST":
         form = SnippetForm(request.POST, instance=snippet)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 _("Success to update the snippet."))
             return redirect('snippet_detail', snippet_id=snippet_id)
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 _("Failed to update the snippet."))
     else:
         form = SnippetForm(instance=snippet)
     return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
+@login_required
 def snippet_detail(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
-    return render(request, 'snippets/snippet_detail.html',
-                  {'snippet': snippet})
+    comments = Comment.objects.filter(commented_to=snippet_id).all()
+    comment_form = CommentForm()
+
+    return render(request, "snippets/snippet_detail.html", {
+        'snippet': snippet,
+        'comments': comments,
+        'comment_form': comment_form,
+    })
 
 
-# def comment_new(request, snippet_id):
-#     snippet = get_object_or_404(Snippet, pk=snippet_id)
-#     return render(request, 'snippets/comments/snippet_detail.html',
-#                   {'snippet': snippet})
+@login_required
+def comment_new(request, snippet_id):
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.commented_to = snippet
+        comment.commented_by = request.user
+        comment.save()
+        messages.add_message(request, messages.SUCCESS,
+                             _("Success to post a comment."))
+    else:
+        messages.add_message(request, messages.ERROR,
+                             _("Failed to post a comment."))
+    return redirect('snippet_detail', snippet_id=snippet_id)
